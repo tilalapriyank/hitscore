bd31b752ee59744e679c008a3eea1bf22d8963bb
tilalapriyank <priyankpatel2764@gmail.com>
2024-12-10 18:31:20 +0530

    score

--- src/pages/match details/MatchScorecard.js
+++ src/pages/match details/MatchScorecard.js
@@ -1,57 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MatchScorecard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      matchDetails: {
-        matchType: 'T20',
-        date: '10 Dec 2024',
-        venue: 'Stadium XYZ',
-        teams: 'Team A vs Team B',
-      },
-      batting: [
-        { player: 'Player 1', runs: 50, balls: 30, fours: 4, sixes: 3, strikeRate: 166.7 },
-        { player: 'Player 2', runs: 20, balls: 15, fours: 2, sixes: 1, strikeRate: 133.3 },
-        { player: 'Player 3', runs: 80, balls: 60, fours: 6, sixes: 4, strikeRate: 133.3 },
-        // More players...
-      ],
-      bowling: [
-        { bowler: 'Bowler 1', overs: 4, runs: 30, wickets: 2, economy: 7.5 },
-        { bowler: 'Bowler 2', overs: 4, runs: 25, wickets: 1, economy: 6.25 },
-        { bowler: 'Bowler 3', overs: 3, runs: 35, wickets: 0, economy: 11.67 },
-        // More bowlers...
-      ],
-      extras: {
-        wides: 5,
-        noBalls: 2,
-        byes: 1,
-      },
-      totalRuns: 200,
-      totalWickets: 5,
-      totalOvers: 20,
+      scoreCard: [],
     };
   }
 
-  render() {
-    const { matchDetails, batting, bowling, extras, totalRuns, totalWickets, totalOvers } = this.state;
+  async componentDidMount() {
+    const { matchId } = this.props; 
+    if (matchId) {
+      const url = `https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/${matchId}/hscard`;
+      const headers = {
+        "X-RapidAPI-Key": "368af94cf6msh643b0a2deb7379dp1c9b02jsn34e51b16fb12",
+        "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
+      };
+      const response = await fetch(url, { headers });
+      const data = await response.json();
+      const scoreCard = data.scoreCard || [];
+      this.setState({ scoreCard });
+    }
+  }
+
+  renderInningDetails(inningData) {
+    const { batTeamDetails, bowlTeamDetails, timeScore } = inningData;
+    const { batsmenData } = batTeamDetails;
+
+    const players = Object.values(batsmenData).map((player) => ({
+      name: player.batName,
+      runs: player.runs || 0, 
+      balls: player.balls || 0, 
+      fours: player.fours || 0, 
+      sixes: player.sixes || 0, 
+      strikeRate: player.strikeRate || 0, 
+      status: player.outDesc || null, 
+    }));
 
     return (
-      <div className="scorecard">
-        {/* Match Details */}
-        <section className="match-details">
-          <h2>Match: {matchDetails.teams}</h2>
-          <p>{matchDetails.date}</p>
-          <p>{matchDetails.matchType} - {matchDetails.venue}</p>
-        </section>
-
-        {/* Batting Stats */}
-        <section className="batting">
-          <h3>Batting</h3>
-          <table>
-            <thead>
+      <div key={inningData.inningsId}>
+        <div className="table-responsive mt-4">
+          <table className="table table-striped">
+            <thead className="scoreboard-header">
               <tr>
                 <th>Player</th>
+                <th>Status</th>
                 <th>Runs</th>
                 <th>Balls</th>
                 <th>4s</th>
@@ -60,64 +53,37 @@ class MatchScorecard extends Component {
               </tr>
             </thead>
             <tbody>
-              {batting.map((batter, index) => (
-                <tr key={index}>
-                  <td>{batter.player}</td>
-                  <td>{batter.runs}</td>
-                  <td>{batter.balls}</td>
-                  <td>{batter.fours}</td>
-                  <td>{batter.sixes}</td>
-                  <td>{batter.strikeRate}</td>
-                </tr>
-              ))}
+              {players.map(
+                (player, index) =>
+                  player.status !== null && (
+                    <tr key={index}>
+                      <td>{player.name}</td>
+                      <td>{player.status || ""}</td>
+                      <td>{player.runs}</td>
+                      <td>{player.balls}</td>
+                      <td>{player.fours}</td>
+                      <td>{player.sixes}</td>
+                      <td>{player.strikeRate}</td>
+                    </tr>
+                  )
+              )}
             </tbody>
           </table>
-        </section>
+        </div>
+      </div>
+    );
+  }
 
-        {/* Extras */}
-        <section className="extras">
-          <h3>Extras</h3>
-          <ul>
-            <li>Wides: {extras.wides}</li>
-            <li>No Balls: {extras.noBalls}</li>
-            <li>Byes: {extras.byes}</li>
-          </ul>
-        </section>
+  render() {
+    const { scoreCard } = this.state;
 
-        {/* Bowling Stats */}
-        <section className="bowling">
-          <h3>Bowling</h3>
-          <table>
-            <thead>
-              <tr>
-                <th>Bowler</th>
-                <th>Overs</th>
-                <th>Runs</th>
-                <th>Wickets</th>
-                <th>Economy</th>
-              </tr>
-            </thead>
-            <tbody>
-              {bowling.map((bowler, index) => (
-                <tr key={index}>
-                  <td>{bowler.bowler}</td>
-                  <td>{bowler.overs}</td>
-                  <td>{bowler.runs}</td>
-                  <td>{bowler.wickets}</td>
-                  <td>{bowler.economy}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </section>
-
-        {/* Match Summary */}
-        <section className="match-summary">
-          <h3>Match Summary</h3>
-          <p>Total Runs: {totalRuns}</p>
-          <p>Total Wickets: {totalWickets}</p>
-          <p>Total Overs: {totalOvers}</p>
-        </section>
+    return (
+      <div className="container mt-4">
+        {scoreCard.length > 0 ? (
+          scoreCard.map((inning) => this.renderInningDetails(inning))
+        ) : (
+          <p>Loading scorecard...</p>
+        )}
       </div>
     );
   }
